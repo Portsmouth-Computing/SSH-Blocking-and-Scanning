4d6d00b885561e35f6523cfacebd142998a213d8
Lagicrus <Lagicrus@users.noreply.github.com>
2017-10-09 16:22:32 +0100

    Sudo test

--- scanner.py
+++ scanner.py
@@ -1,6 +1,12 @@
 import os, sys, time
 
+if os.geteuid() != 0:
+    print("Please run as sudo.")
+    exit()
+
+
 with open("/var/log/auth.log") as file:
     for line in file:
         print(line)
         time.sleep(1)
+
