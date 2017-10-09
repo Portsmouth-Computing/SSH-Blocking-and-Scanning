df1f9d1e2fee816148c8a1c4da4038632c7910fb
Lagicrus <Lagicrus@users.noreply.github.com>
2017-10-09 20:26:52 +0100

    api added and reducing the verbose of IPs already checked

--- scanner.py
+++ scanner.py
@@ -1,4 +1,4 @@
-import os, time, pickle
+import os, time, pickle, requests
 
 if os.geteuid() != 0:
     print("Please run as sudo.")
@@ -27,6 +27,7 @@ except UnicodeDecodeError as UDE:
     print(UDE, ". Loading blank IP list")
     ip_list = []
 
+ip_temp_list = []
 
 ip_stats = {}
 
@@ -37,13 +38,14 @@ with open("/var/log/auth.log") as file:
         if "Failed password for root from" in line.strip():
             if line_split[8] == "root":
 
-                if line_split[10] not in ip_list:
+                if line_split[10] not in ip_list or line_split[10] not in ip_temp_list:
+                    if line_split not in ip_temp_list:
+                        print("Adding {} to list".format(line_split[10]))
                     ip_stats[line_split[10]] = 1
-                    print("Adding {} to list".format(line_split[10]))
                     if line_split[10] != "root":
-                        ip_list.append(line_split[10])
-                        with open(working_file, "wb")as pickled:
-                            pickle.dump(ip_list, pickled)
+                        ip_temp_list.append(line_split[10])
+                        # with open(working_file, "wb")as pickled:
+                        #     pickle.dump(ip_list, pickled)
                         print("Saved")
                     else:
                         print(line_split)
@@ -58,6 +60,22 @@ with open("/var/log/auth.log") as file:
                         time.sleep(0.5)
 
     #        time.sleep(1)
+        elif "Unable to negotiate with" in line.strip():
+            if line_split[9] not in ip_list:
+                print("Adding {} to list from key exchange".format(line_split[9]))
+                ip_list.append(line_split[9])
         # Also work on a statement that checks lines like `Oct  8 17:27:33 up857256 sshd[15848]: Unable to negotiate with 27.76.249.209 port 56038: no matching key exchange method found. Their offer: diffie-hellman-group1-sha1 [preauth]`
 
 print(ip_stats)
+
+for ip in ip_temp_list:
+    r = requests.get("https://www.ipinfo.io/{}/country".format(ip))
+    if r.status_code == 200:
+        print("This IP came from {} ({})".format(r.text.strip(),ip))
+        ip_list.append()
+        with open(working_file,"wb") as pickled:
+            pickle.dump(ip_list,pickled)
+    elif r.status_code == 429:
+        print("Rate limited. Please wait for 24hrs.")
+        input(">")
+        exit()
