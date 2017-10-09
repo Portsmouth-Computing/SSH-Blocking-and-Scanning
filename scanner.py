2ae3a9e12b873fbf8d1a2ba33458e97691f22eb4
Lagicrus <Lagicrus@users.noreply.github.com>
2017-10-09 17:23:10 +0100

    IP Counter
    IP counter to count the amount of times a IP is detected. Main reason
    was to be less verbose though this can now be used to count the stats of
    each IP before being blocked.

--- scanner.py
+++ scanner.py
@@ -17,23 +17,30 @@ try:
     else:
         ip_list = []
 except UnicodeDecodeError as UDE:
-    print(UDE,"Loading blank IP list")
+    print(UDE, ". Loading blank IP list")
     ip_list = []
+
+
+ip_stats = {}
 
 with open("/var/log/auth.log") as file:
     for line in file:
         # print(line.strip().split())
-        was = line.strip().split()
+        line_split = line.strip().split()
         if "root" in line.strip():
-            if was[8] == "root":
-                print("Failed root from: {}".format(was[10]))
+            if line_split[8] == "root":
 
-                if was[10] not in ip_list:
-                    print("Adding {} to list".format(was[10]))
-                    ip_list.append(was[10])
-                    with open("~/SSH-Blocking-and-Scanning/auth_scanning.pickle","wb")as pickled:
+                if line_split[10] not in ip_list:
+                    ip_stats[line_split[10]] = 0
+                    print("Adding {} to list".format(line_split[10]))
+                    ip_list.append(line_split[10])
+                    with open("~/SSH-Blocking-and-Scanning/auth_scanning.pickle", "wb")as pickled:
                         pickle.dump(ip_list, pickled)
                     print("Saved")
+                else:
+                    ip_stats[line_split[10]] += 1
+                    if ip_stats[line_split[10]] <= 10:
+                        print("Failed root from: {}".format(line_split[10]))
 
                 time.sleep(0.5)
     #        time.sleep(1)
