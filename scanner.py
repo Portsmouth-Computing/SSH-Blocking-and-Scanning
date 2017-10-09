ee833174364672b616ac2b2c4828de2c4731a3af
Lagicrus <Lagicrus@users.noreply.github.com>
2017-10-09 16:27:52 +0100

    Removing whitespace on printing

--- scanner.py
+++ scanner.py
@@ -3,10 +3,12 @@ import os, sys, time
 if os.geteuid() != 0:
     print("Please run as sudo.")
     exit()
+else:
+    print("Running as sudo.")
 
 
 with open("/var/log/auth.log") as file:
     for line in file:
-        print(line)
+        print(line.strip())
         time.sleep(1)
 
