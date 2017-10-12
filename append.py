517ef4fadeb4fea7ec0d881b3bf722a488e32d51
Lagicrus <Lagicrus@users.noreply.github.com>
2017-10-12 23:16:34 +0100

    bulk work

--- append.py
+++ append.py
@@ -10,6 +10,34 @@ if os.geteuid() != 0:
 else:
     print("Running as sudo.")
 
+staticInfo = ["\n","####### This section was created by a program to block irritating IP's","\n"]
+
 username = os.getlogin()
 
-"/home/{}/20fdd6a36582ad545e91485592c8ab4e"
+hosts_file = []
+hosts_counter = 0
+with open("/etc/hosts.deny") as hosts_filea:
+    for line in hosts_filea:
+        if hosts_counter != 17:
+            hosts_file.append("{}\n".format(line.strip()))
+            hosts_counter += 1
+        else:
+            break
+
+sshd_ip_list = []
+with open("./ip_raw.txt") as rawIPs:
+    for line in rawIPs:
+        sshd_ip_list.append("{}\n".format(line.strip()))
+
+tempList = []
+
+for line in hosts_file:
+    tempList.append(line)
+for line in staticInfo:
+    tempList.append(line)
+for line in sshd_ip_list:
+    tempList.append(line)
+
+with open("/etc/hosts.deny", "w") as hosts_filea:
+    for line in tempList:
+        hosts_filea.write(line)
