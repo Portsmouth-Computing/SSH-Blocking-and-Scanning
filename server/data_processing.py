ee41568aafe43dfff7b064970b905d3cd6839b58
lagicrus <Lagicrus@users.noreply.github.com>
2018-07-31 16:15:08 +0100

    Added data_processing

---
+++ server/data_processing.py
@@ -0,0 +1,18 @@
+import database_programs
+
+
+def processing_list(ip_list, conn):
+    for ip in ip_list:
+        if ip not in ip_list and ip not in ip_country_stats["IP_Stats"]:
+            r = requests.get("https://www.ipinfo.io/{}/country".format(ip))
+            if r.status_code == 200:
+
+                ip_country_stats["IP_Stats"][ip] = r.text.strip()
+
+                ip_list.append(ip)
+                with open(working_file, "wb") as pickled:
+                    pickle.dump(ip_list, pickled)
+                with open(ip_tracker_file, "wb") as pickled:
+                    pickle.dump(ip_country_stats, pickled)
+            elif r.status_code == 429:
+                break
