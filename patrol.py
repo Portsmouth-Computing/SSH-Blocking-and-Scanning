3ca8c07bcaf13633647cdb85321fcfc7c99da066
lagicrus <Lagicrus@users.noreply.github.com>
2018-08-02 21:09:13 +0100

    removed unneeded code

--- patrol.py
+++ patrol.py
@@ -6,11 +6,6 @@ def main():
         print("Not root. Need root perms to access auth.log")
         exit()
 
-    username = os.getlogin()
-
-    bad_origin = ["CN", "KR", "TR", "VN", "RU", "BR", "undefined", "MX", "ID", "IT", "HK"]
-
-    ip_list = []
     ip_temp_list = []
 
     with open("/var/log/auth.log") as file:
@@ -24,7 +19,7 @@ def main():
             if "Failed password for invalid user" in line.strip():
                 if not line_split[10].lower().startswith("up"):
                     failed_user_ip = line_split[12]
-                    if failed_user_ip not in ip_list or failed_user_ip not in ip_temp_list:
+                    if failed_user_ip not in ip_temp_list:
                         ip_temp_list.append(failed_user_ip)
 
             #if "Failed password for" in line.strip():
@@ -35,12 +30,12 @@ def main():
 
             elif "Unable to negotiate with" in line.strip():
                 negotiate_ip = line_split[9]
-                if negotiate_ip not in ip_list and negotiate_ip not in ip_temp_list:
+                if negotiate_ip not in ip_temp_list:
                     ip_temp_list.append(negotiate_ip)
 
             elif "Did not receive identification string from" in line.strip():
                 identification_ip = line_split[11]
-                if identification_ip not in ip_list and identification_ip not in ip_temp_list:
+                if identification_ip not in ip_temp_list:
                     ip_temp_list.append(identification_ip)
 
     return ip_temp_list
