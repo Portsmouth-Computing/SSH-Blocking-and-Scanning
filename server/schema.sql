0c87489c4ada8f05354095ea1def84be3fb4cc98
lagicrus <Lagicrus@users.noreply.github.com>
2018-08-02 22:43:43 +0100

    commas

--- server/schema.sql
+++ server/schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS ip_storage (
     ip CIDR PRIMARY KEY,
     country_code TEXT CHECK (char_length(country_code) = 2),
 
-    accessed BIGINT DEFAULT 1
-    last_updated TIMESTAMP DEFAULT now(),
+    accessed BIGINT DEFAULT 1,
+    last_updated TIMESTAMP DEFAULT now()
 );
