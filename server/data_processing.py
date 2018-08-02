d3462d46aa81b0ef1d1c297236e72aa90b52a01e
lagicrus <Lagicrus@users.noreply.github.com>
2018-08-03 00:39:17 +0100

    Attempt to fix bug

--- server/data_processing.py
+++ server/data_processing.py
@@ -13,12 +13,14 @@ async def single_ip_processing(ip, conn, app_session):
     ip_info = await database_programs.fetch_from_database(conn, ip)
     if ip_info is not None:
         await database_programs.update_entry(conn, ip, ip_info["accessed"])
-        return await database_programs.fetch_formattor(ip_info)
+        # return await database_programs.fetch_formattor(ip_info)
+        return dict(ip_info)
     else:
         country = await single_data_retrieval(ip, app_session)
         await database_programs.insert_into_database(conn, ip, country)
         ip_info = await database_programs.fetch_from_database(conn, ip)
-        return await database_programs.fetch_formattor(ip_info)
+        # return await database_programs.fetch_formattor(ip_info)
+        return dict(ip_info)
 
 
 async def processing_list(ip_list, conn):
