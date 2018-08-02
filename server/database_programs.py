144f6e64530007bafbf144a471f74a83be63eb9e
lagicrus <Lagicrus@users.noreply.github.com>
2018-08-02 23:42:47 +0100

    renaming

--- server/database_programs.py
+++ server/database_programs.py
@@ -21,9 +21,9 @@ async def fetch_formattor(ip_dict):
 async def update_entry(conn, ip, current_total):
     await conn.execute("""
     UPDATE ip_storage
-        SET amount_checked = amount_checked + 1
+        SET accessed = accessed + 1
     WHERE ip = $1
-        AND amount_checked = $2
+        AND accessed = $2
     """, ip, current_total)
 
 
