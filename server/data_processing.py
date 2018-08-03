85b8281f5d348740c0884af57867fbf3354d107e
lagicrus <Lagicrus@users.noreply.github.com>
2018-08-03 17:43:48 +0100

    Making sure we dont spam 429

--- server/data_processing.py
+++ server/data_processing.py
@@ -11,29 +11,40 @@ async def single_data_retrieval(ip, app_session):
     return country, status
 
 
-async def single_ip_processing(ip, conn, app_session):
+async def single_ip_processing(ip, conn, app_session, limit_hit=False):
     ip_info = await database_programs.fetch_from_database(conn, ip)
 
-    print(ip, conn, ip_info)
     if ip_info is not None:
         await database_programs.update_entry(conn, ip, ip_info["accessed"])
         return await database_programs.fetch_formattor(ip_info)
 
     else:
-        country, status = await single_data_retrieval(ip, app_session)
-        if status == 200:
-            await database_programs.insert_into_database(conn, ip, country)
-            ip_info = await database_programs.fetch_from_database(conn, ip)
-            return await database_programs.fetch_formattor(ip_info)
+
+        if not limit_hit:
+            country, status = await single_data_retrieval(ip, app_session)
+
+            if status == 200:
+                await database_programs.insert_into_database(conn, ip, country)
+                ip_info = await database_programs.fetch_from_database(conn, ip)
+                return await database_programs.fetch_formattor(ip_info)
+
+            else:
+                print(f"{status} for {ip}")
+                return {"ip": ip, "country": "??", "last_updated": time(), "amount_checked": 1}
+
         else:
             return {"ip": ip, "country": "??", "last_updated": time(), "amount_checked": 1}
 
 
 async def processing_list(ip_list, conn, app_session):
     processed_ip_list = []
+    limit_hit = False
 
     for ip in ip_list:
-        processed_ip_list.append(await single_ip_processing(ip, conn, app_session))
+        ip_info = await single_ip_processing(ip, conn, app_session, limit_hit)
+        if ip_info["country"] == "??":
+            limit_hit = True
+        processed_ip_list.append(ip_info)
 
     return processed_ip_list
 
