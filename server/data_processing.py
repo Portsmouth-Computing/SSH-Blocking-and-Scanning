9523344ec73deaf7a7b0208328c91ccbbddb15d8
Lagicrus <Lagicrus@users.noreply.github.com>
2018-08-12 04:17:45 +0100

    Bug fix
    Aiohttp gets seen as a browser by the API responding with a HTML site instead of JSON data
    If you do /json it forces it to be JSON

--- server/data_processing.py
+++ server/data_processing.py
@@ -5,7 +5,7 @@ import itertools
 
 
 async def single_data_retrieval(ip, app_session):
-    async with app_session.get(f"http://www.ipinfo.io/{ip}") as resp:
+    async with app_session.get(f"http://www.ipinfo.io/{ip}/json") as resp:
         json = await resp.json()
         status = resp.status
     country = json["country"].strip()
@@ -124,35 +124,3 @@ async def processing_list(ip_list, conn, app_session):
 
     return processed_ip_list
 
-"""
-        ip_info = await database_programs.fetch_from_database(conn, ip)
-
-        if ip_info is not None:
-            await database_programs.update_entry(conn, ip, ip_info["accessed"])
-            processed_ip_list.append(await database_programs.fetch_formattor(ip_info))
-
-        else:
-            country = await single_data_retrieval(ip, app_session)
-            await database_programs.insert_into_database(conn, ip, country)
-            ip_info = await database_programs.fetch_from_database(conn, ip)
-            processed_ip_list.append(await database_programs.fetch_formattor(ip_info))
-
-    return processed_ip_list
-    """
-
-"""
-    for ip in ip_list:
-        if ip not in ip_list and ip not in ip_country_stats["IP_Stats"]:
-            r = requests.get("https://www.ipinfo.io/{}/country".format(ip))
-            if r.status_code == 200:
-
-                ip_country_stats["IP_Stats"][ip] = r.text.strip()
-
-                ip_list.append(ip)
-                with open(working_file, "wb") as pickled:
-                    pickle.dump(ip_list, pickled)
-                with open(ip_tracker_file, "wb") as pickled:
-                    pickle.dump(ip_country_stats, pickled)
-            elif r.status_code == 429:
-                break
-"""
