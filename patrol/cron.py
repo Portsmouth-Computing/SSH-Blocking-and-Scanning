42a0abe93b16a8c9c3a46d4bd8e77c0e1cd16585
Lagicrus <Lagicrus@users.noreply.github.com>
2019-01-31 02:30:36 +0000

    Revert "Update cron.py to optimise checking"
    This reverts commit 3ce6f8358ae7b1760555236c819f308beabc4707.

--- patrol/cron.py
+++ patrol/cron.py
@@ -4,9 +4,24 @@ import subprocess
 import re
 
 SAFE_LIST = ["GB"]
-static_info = ["\n", "####### This section was created by a program to block irritating IP's", "\n"]
 UNSAFE_IP_LIST = []
 re_luma = re.compile(r'(?P<address>(?:[a-f\d]{1,4}[\.:]){1,7}[a-f\d]{1,4})')
+
+ip_list = patrol.main()
+process_list = requests.get("https://api.oceanlord.me/ip/info", json={"addresses": ip_list})
+
+if process_list.status_code != 200:
+    print(process_list.status_code, process_list.text)
+    exit()
+
+for ip_address in list(process_list.json().keys()):
+    if ip_address not in UNSAFE_IP_LIST:
+        if process_list.json()[ip_address]["country_code"] not in SAFE_LIST and not process_list.json()[ip_address]["private"]:
+            UNSAFE_IP_LIST.append(ip_address)
+
+
+static_info = ["\n", "####### This section was created by a program to block irritating IP's", "\n"]
+hosts_file_contents = []
 
 line_counter = 0
 info_line = 0
@@ -33,29 +48,6 @@ with open("/etc/hosts.deny") as file:
                     print(temp_line, re_luma.search(temp_line))
         else:
             line_counter += 1
-
-ip_list = patrol.main()
-
-new_ip_list = []
-for ip in ip_list:
-    if ip not in BACKED_UP_IP_LIST:
-        new_ip_list.append(ip)
-
-process_list = requests.get("https://api.oceanlord.me/ip/info", json={"addresses": new_ip_list})
-
-if process_list.status_code != 200:
-    print(process_list.status_code, process_list.text)
-    exit()
-
-for ip_address in list(process_list.json().keys()):
-    if ip_address not in UNSAFE_IP_LIST:
-        if process_list.json()[ip_address]["country_code"] not in SAFE_LIST and not process_list.json()[ip_address]["private"]:
-            UNSAFE_IP_LIST.append(ip_address)
-
-
-
-hosts_file_contents = []
-
 
 SET_IP_LIST = set(BACKED_UP_IP_LIST + UNSAFE_IP_LIST)
 
