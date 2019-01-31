eaf83f473c55530e122317ac6f85fcb4e3356f53
Lagicrus <Lagicrus@users.noreply.github.com>
2019-01-31 12:42:31 +0000

    attempt to optimise

--- patrol/cron.py
+++ patrol/cron.py
@@ -1,32 +1,24 @@
-import patrol
-import requests
-import subprocess
 import re
+import subprocess
+
+import requests
+
+import patrol
 
 SAFE_LIST = ["GB"]
 UNSAFE_IP_LIST = []
 re_luma = re.compile(r'(?P<address>(?:[a-f\d]{1,4}[\.:]){1,7}[a-f\d]{1,4})')
-
-ip_list = patrol.main()
-process_list = requests.get("https://api.oceanlord.me/ip/info", json={"addresses": ip_list})
-
-if process_list.status_code != 200:
-    print(process_list.status_code, process_list.text)
-    exit()
-
-for ip_address in list(process_list.json().keys()):
-    if ip_address not in UNSAFE_IP_LIST:
-        if process_list.json()[ip_address]["country_code"] not in SAFE_LIST and not process_list.json()[ip_address]["private"]:
-            UNSAFE_IP_LIST.append(ip_address)
-
-
 static_info = ["\n", "####### This section was created by a program to block irritating IP's", "\n"]
 hosts_file_contents = []
-
 line_counter = 0
 info_line = 0
 hosts_file_storage = []
 BACKED_UP_IP_LIST = []
+
+ip_list = patrol.main()
+print("IP LIST")
+print(ip_list)
+print(len(ip_list))
 
 with open("/etc/hosts.deny") as file:
     for line in file:
@@ -36,6 +28,7 @@ with open("/etc/hosts.deny") as file:
         else:
             hosts_file_storage.append(line)
             line_counter += 1
+with open("/etc/hosts.deny") as file:
     line_counter = 0
     for line in file:
         if line_counter > info_line:
@@ -48,9 +41,34 @@ with open("/etc/hosts.deny") as file:
                     print(temp_line, re_luma.search(temp_line))
         else:
             line_counter += 1
+print("BACKED UP")
+print(sorted(BACKED_UP_IP_LIST))
+print(len(BACKED_UP_IP_LIST))
+
+improved_ip_list = []
+for ip in ip_list:
+    if ip not in BACKED_UP_IP_LIST:
+        improved_ip_list.append(ip)
+
+process_list = requests.get("https://api.oceanlord.me/ip/info", json={"addresses": improved_ip_list})
+
+if process_list.status_code != 200:
+    print(process_list.status_code, process_list.text)
+    exit()
+
+for ip_address in list(process_list.json().keys()):
+    if ip_address not in UNSAFE_IP_LIST:
+        if process_list.json()[ip_address]["country_code"] not in SAFE_LIST and not process_list.json()[ip_address]["private"]:
+            UNSAFE_IP_LIST.append(ip_address)
 
 SET_IP_LIST = set(BACKED_UP_IP_LIST + UNSAFE_IP_LIST)
-
+print("SET IP")
+print(len(SET_IP_LIST))
+temp_ip = []
+for ip in ip_list:
+    if ip not in BACKED_UP_IP_LIST:
+        temp_ip.append(ip)
+print(len(temp_ip))
 with open("/etc/hosts.deny", "w") as file:
     for line in hosts_file_storage:
         file.write(line)
